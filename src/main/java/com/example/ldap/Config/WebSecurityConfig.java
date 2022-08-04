625ad11a60fbf082e5f4da52b70b163d53e2c2cf
math <ytlin10@gmail.com>
2022-08-04 12:13:34 +0800

    Initial commit

--- src/main/java/com/example/ldap/Config/WebSecurityConfig.java
+++ src/main/java/com/example/ldap/Config/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.ldap.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,9 +22,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin();
     }
 
+    @Bean
+    public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+
+        ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider =
+                new ActiveDirectoryLdapAuthenticationProvider( "math.sinica", "ldap://192.168.236.11");
+
+        // to parse AD failed credentails error message due to account - expiry,lock, credentialis - expiry,lock
+        activeDirectoryLdapAuthenticationProvider.setConvertSubErrorCodesToExceptions(true);
+
+        return activeDirectoryLdapAuthenticationProvider;
+    }
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
+/*        auth
                 .ldapAuthentication()
                 .userDnPatterns("uid={0},ou=people")
                 .groupSearchBase("ou=groups")
@@ -29,5 +45,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordCompare()
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
+
+ */
+        auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider());
+
     }
 }
