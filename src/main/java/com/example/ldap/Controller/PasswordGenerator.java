5abd008933831ad097d7dbd0e88874883ed7b564
math <ytlin10@gmail.com>
2022-08-04 11:41:00 +0800

    Initial commit

---
+++ src/main/java/com/example/ldap/Controller/PasswordGenerator.java
@@ -0,0 +1,15 @@
+package com.example.ldap.Controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+
+   public static void main(String[] args){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "ben";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+    }
+
+}
